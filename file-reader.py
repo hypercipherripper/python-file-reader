9201b1e9dd4568c864b42658627110103608d906
Hoja Arzanesh <hypercipherripper@gmail.com>
2024-08-05 03:14:03 -0400

    Add line_splitter function. Given a line, splits the line by space, and returns the result array

--- file-reader.py
+++ file-reader.py
@@ -4,3 +4,11 @@ def lines_extractor(filepath):
     with open(filepath) as f:
         return f.readlines()
 
+
+# given a line, splits the line by space, and returns the result
+# array
+def line_splitter(line):
+    return line.split()
+
+
+
